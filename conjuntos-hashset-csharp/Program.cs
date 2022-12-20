50d1dfb608c9af183a53cf2632b206c12bc29c6d
matheus.silva <tecmatheusg@hotmail.com>
2022-12-20 11:52:25 -0300

    Primeiro commit

---
+++ conjuntos-hashset-csharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace conjuntos_hashset_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\Users\matheus.silva\Documents\ProjetosPessoais\CSharp\conjuntos-hashset-csharp\conjuntos-hashset-csharp\doc1.csv";
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    HashSet<RegistroLogin> listaDeRegistos = new HashSet<RegistroLogin>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string linha = sr.ReadLine();
+                        RegistroLogin reg = new RegistroLogin(linha);
+                        listaDeRegistos.Add(reg);
+                    }
+
+                    Console.WriteLine(listaDeRegistos.Count);
+                    /*
+                    var contador = 0;
+                    foreach (var registro in listaDeRegistos)
+                    {
+                        if (listaDeRegistos.Contains(registro))
+                        {
+                            contador++;
+                        }
+                    }
+                    Console.WriteLine(contador);
+                    */
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ conjuntos-hashset-csharp/RegistroLogin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conjuntos_hashset_csharp
+{
+    public class RegistroLogin
+    {
+        public string Nome { get; set; }
+        public DateTime HoraLogin { get; set; }
+        public RegistroLogin(string linha)
+        {
+            var reg = linha.Split(" ");
+            Nome = reg[0];
+            HoraLogin = DateTime.Parse(reg[1]);
+        }
+
+        
+        public override bool Equals(object obj)
+        {
+            return obj is RegistroLogin login &&
+                   Nome == login.Nome;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Nome);
+        }
+    }
+}
